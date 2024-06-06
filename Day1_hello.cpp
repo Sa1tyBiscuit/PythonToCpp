e12db2b96fe4a1883b3aad2d2d148ecb4e90e2b6
Sa1tyBiscuit <58919516+Sa1tyBiscuit@users.noreply.github.com>
2024-06-06 20:55:05 +0800

    commit day one cpp file

---
+++ Day1_hello.cpp
@@ -0,0 +1,21 @@
+// classical beginning to a hello world program but in cpp 
+// python equivalent of print("hello world")
+
+//import the standard library from cpp 
+
+// to run the file 
+/*  
+g++ -o helloworld Day1_hello.cpp
+compile -o <output_filename> <filename>.cpp
+*/
+
+//import from c++ standard library
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World! \n"; // << is the output operator
+    return 0; // semicolons tell the compiler where statements begin/end
+}
+
